d4b4e76c1851e091fcd84d9aa6a2cf6bdf2d7341
Rumit Singh Tuteja <rtuteja@i-360.com>
2024-06-04 10:58:22 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Rumit Singh Tuteja on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
